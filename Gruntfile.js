60c563b991dfdbedcbf5a40d2931ef92347c9419
Dan Mosedale <dmose@meer.net>
2013-12-26 03:52:11 -0800

    Git rid of coffeescript glop

--- Gruntfile.js
+++ Gruntfile.js
@@ -122,26 +122,6 @@ module.exports = function (grunt) {
         }
       }
     },
-    coffee: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/scripts',
-          src: '{,*/}*.coffee',
-          dest: '.tmp/scripts',
-          ext: '.js'
-        }]
-      },
-      test: {
-        files: [{
-          expand: true,
-          cwd: 'test/spec',
-          src: '{,*/}*.coffee',
-          dest: '.tmp/spec',
-          ext: '.js'
-        }]
-      }
-    },
     compass: {
       options: {
         sassDir: '<%= yeoman.app %>/styles',
@@ -309,7 +289,6 @@ module.exports = function (grunt) {
         'copy:styles'
       ],
       test: [
-        'coffee',
         'copy:styles'
       ],
       dist: [
