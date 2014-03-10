dfe337b0d1f4221bd868892fd1d57d1842d88968
Dan Mosedale <dmose@meer.net>
2014-03-09 18:51:57 -0600

    Actually checkin the polyfills-pkg bower component

---
+++ test/bower_components/polyfills-pkg/src/wrap/preamble.js
@@ -0,0 +1,9 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else {
+        // Browser globals
+        root.amdWeb = factory();
+    }
+}(this, function () {
